880156ed22fc6451d50a433441726ea6037b1fdb
Egor Pobylets <egorpobylets@Egors-MacBook-Air.local>
2023-09-15 09:08:55 +0200

    fix: remove comment

--- src/db/db.ts
+++ src/db/db.ts
@@ -12,20 +12,3 @@ export const db = <T,>(clearData?: boolean): Record<string, T[]> => {
 
   return dbData
 }
-// export const db = <T,>(data?: string, clearData?: boolean): Record<string, T[]> => {  
-//   if (data && !dbData[data]) {
-//     const qwe = {
-//       ...dbData,
-//       [data]: []
-//     } as any
-
-//     dbData = qwe
-
-//     return {
-//       ...dbData,
-//       [data]: []
-//     } as any
-//   }
-
-//   return dbData
-// }
