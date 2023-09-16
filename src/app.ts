c72698ca81d07c2280e21b8d84b9f61a5ec0f369
Egor Pobylets <egorpobylets@Egors-MacBook-Air.local>
2023-09-16 13:38:52 +0200

    fix: fix erros

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,5 @@ import { videosRouter, globalRouter } from './routes'
 export const app = express()
 app.use(bodyParser.json())
 
-app.use('/videos', videosRouter)
 app.use('/all-data', globalRouter)
+app.use('/videos', videosRouter)
