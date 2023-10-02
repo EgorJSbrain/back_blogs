b2e512d9676ab52bf577cbf6688fe04379a3ae45
Egor Pobylets <egorpobylets@gmail.com>
2023-10-02 08:29:47 +0200

    feat: add request for creating post by blogId

--- src/services/blogs.ts
+++ src/services/blogs.ts
@@ -1,9 +1,12 @@
-import { generateNewBlog } from './utils'
+import { generateNewBlog, generateNewPost } from './utils'
 import { BlogsRepository, PostsRepository } from '../repositories'
 
 import { CreateBlogDto } from '../dtos/blogs/create-blog.dto'
 import { UpdateBlogDto } from '../dtos/blogs/update-blog.dto'
-import { BlogPostsRequestParams, BlogsRequestParams } from '../types/blogs'
+import { BlogPostsRequestParams, BlogsRequestParams, IBlog } from '../types/blogs'
+import { BlogInputFields } from '../constants/blogs'
+import { PostInputFields } from '../constants/posts'
+import { CreatePostDto } from '../dtos/posts/create-post.dto'
 
 export const BlogsService = {
   async getBlogs(params: BlogsRequestParams) {
@@ -15,7 +18,14 @@ export const BlogsService = {
   },
 
   async createBlog(data: CreateBlogDto) {
-    const createdBlog = generateNewBlog(data)
+    const { name, description, websiteUrl } = data
+
+    const creatingData = {
+      [BlogInputFields.name]: name,
+      [BlogInputFields.description]: description,
+      [BlogInputFields.websiteUrl]: websiteUrl
+    }
+    const createdBlog = generateNewBlog(creatingData)
 
     return await BlogsRepository.createBlog(createdBlog)
   },
@@ -30,5 +40,21 @@ export const BlogsService = {
 
   async getPostsByBlogId(params: BlogPostsRequestParams) {
     return await PostsRepository.getPostsByBlogId(params)
+  },
+
+  async createPostByBlogId(data: CreatePostDto, blog: IBlog) {
+    const { title, shortDescription, content } = data
+
+    const creatingData = {
+      [PostInputFields.title]: title,
+      [PostInputFields.shortDescription]: shortDescription,
+      [PostInputFields.content]: content,
+      [PostInputFields.blogId]: blog.id,
+      [PostInputFields.blogName]: blog?.name ?? ''
+    }
+
+    const createdPost = generateNewPost(creatingData)
+
+    return await PostsRepository.createPostByBlogId(createdPost)
   }
 }
