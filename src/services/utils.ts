9412663868a8c176951691e48770227715f73bc5
Egor Pobylets <egorpobylets@gmail.com>
2023-11-02 11:13:00 +0100

    feat: rewrote tokens to devices

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,10 +1,5 @@
-import { v4 } from 'uuid'
-import add from 'date-fns/add'
-
 import { IVideo } from '../types/videos'
 import { CreateVideoDto } from '../dtos/videos/create-video.dto'
-import { IRefreshTokenMeta } from '../types/devices'
-import { CreateRefreshTokenDto } from '../dtos/tokens/create-refresh-token.dto'
 
 export const generateNewVideo = (data: CreateVideoDto): IVideo => {
   const createdDate = Number(new Date()) - 1000 * 60 * 60 * 24
@@ -18,22 +13,5 @@ export const generateNewVideo = (data: CreateVideoDto): IVideo => {
     canBeDownloaded: data.canBeDownloaded || false,
     createdAt: new Date(createdDate).toISOString(),
     publicationDate: new Date().toISOString()
-  }
-}
-
-export const generateNewRefreshToken = (
-  data: CreateRefreshTokenDto
-): IRefreshTokenMeta => {
-  const timeStamp = new Date()
-
-  return {
-    ip: data.ip,
-    userId: data.userId,
-    deviceId: v4(),
-    title: data.title,
-    lastActiveDate: timeStamp.toISOString(),
-    expiredDate: add(timeStamp, {
-      seconds: 20
-    }).toISOString()
   }
 }
