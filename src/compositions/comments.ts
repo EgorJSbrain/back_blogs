313e613ec02a5874c02501c96b9f044685511ccc
Egor Pobylets <egorpobylets@gmail.com>
2023-11-07 07:54:11 +0100

    fix: fix likes for comments

--- src/compositions/comments.ts
+++ src/compositions/comments.ts
@@ -3,8 +3,14 @@ import { CommentsService } from '../services'
 import { CommentsController } from '../controllers/comments'
 import { usersService } from './users'
 import { likesService } from './likes'
+import { jwtService } from '../applications/jwt-service'
 
 const commentsRepository = new CommentsRepository()
 export const commentsService = new CommentsService(commentsRepository)
 
-export const commentsController = new CommentsController(commentsService, usersService, likesService)
+export const commentsController = new CommentsController(
+  commentsService,
+  usersService,
+  likesService,
+  jwtService
+)
