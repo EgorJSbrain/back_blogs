cd433b6e3c5781998efbf887e92f09724074c1e9
Egor Pobylets <egorpobylets@gmail.com>
2023-11-12 15:30:24 +0100

    fix: fixed filter for posts in blogs

--- src/compositions/blogs.ts
+++ src/compositions/blogs.ts
@@ -2,8 +2,9 @@ import { BlogsRepository } from '../repositories'
 import { BlogsController } from '../controllers/blogs'
 import { BlogsService } from '../services'
 import { postsService } from './posts'
+import { jwtService } from '../applications/jwt-service'
 
 const blogsRepository = new BlogsRepository()
 export const blogsService = new BlogsService(blogsRepository, postsService)
 
-export const blogsController = new BlogsController(blogsService, postsService)
+export const blogsController = new BlogsController(blogsService, postsService, jwtService)
